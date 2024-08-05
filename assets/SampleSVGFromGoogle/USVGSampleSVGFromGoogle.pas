0c121a0d3650652b30061dafda3328a93901dd94
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-08-05 20:33:08 +0200

    added header copyright comments

--- assets/SampleSVGFromGoogle/USVGSampleSVGFromGoogle.pas
+++ assets/SampleSVGFromGoogle/USVGSampleSVGFromGoogle.pas
@@ -1,3 +1,55 @@
+﻿/// <summary>
+/// ***************************************************************************
+///
+/// SVG folder to Delphi unit
+///
+/// Copyright 2024 Patrick Prémartin under AGPL 3.0 license.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+/// DEALINGS IN THE SOFTWARE.
+///
+/// ***************************************************************************
+///
+/// This program is designed for Delphi developers wishing to use vector
+/// images (in SVG format) into their projects.
+///
+/// It generates a unit from the list of SVG files contained in a folder.
+///
+/// An SVG list is created as an array of multilines strings containing the
+/// textual source code of each vector image.
+///
+/// Constants and an enumeration containing the unit name and the SVG file
+/// names provide the indices of the associated source code in the list. This
+/// makes it easier to find your way around and manage several SVG lists in a
+/// single project.
+///
+/// You can use the result in a VCL or FireMonkey Delphi project, with Skia
+/// enabled (via Skia4Delphi project or natively) and the unit
+/// Olf.Skia.SVGToBitmap.pas from
+/// https://github.com/DeveloppeurPascal/librairies/
+///
+/// ***************************************************************************
+///
+/// Author(s) :
+/// Patrick PREMARTIN
+///
+/// Site :
+/// https://svgfolder2delphiunit.olfsoftware.fr/
+///
+/// Project site :
+/// https://github.com/DeveloppeurPascal/SVGFolder2DelphiUnit
+///
+/// ***************************************************************************
+/// File last update : 2024-07-28T14:24:54.634+02:00
+/// Signature : 1cad3153e952b31c15278a60126734c3c599af15
+/// ***************************************************************************
+/// </summary>
+
 unit USVGSampleSVGFromGoogle;
 
 // ****************************************
