e5e5174f54eabcaeaf7a6b5997243699db02738a
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-08-06 19:33:42 +0200

    added the license text

---
+++ src/uAboutLicenseText.pas
@@ -0,0 +1,14 @@
+﻿unit uAboutLicenseText;
+
+interface
+
+const
+  CAboutLicenseEN = '''
+This program is distributed as shareware. If you use it (especially for commercial or income-generating purposes), please remember the author and contribute to its development by purchasing a license.
+
+This software is supplied as is, with or without bugs. No warranty is offered as to its operation or the data processed. Make backups!
+''';
+
+implementation
+
+end.
