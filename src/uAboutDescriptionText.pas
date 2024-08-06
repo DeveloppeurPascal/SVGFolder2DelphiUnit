ad68cef42acb01273aa360be869ac7511074ea1b
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-08-06 19:33:50 +0200

    added the description text

---
+++ src/uAboutDescriptionText.pas
@@ -0,0 +1,43 @@
+unit uAboutDescriptionText;
+
+interface
+
+const
+  CAboutDescriptionEN = '''
+This program is designed for Delphi developers wishing to use vector images (in SVG format) into their projects.
+
+It generates a unit from the list of SVG files contained in a folder.
+
+An SVG list is created as an array of multilines strings containing the textual source code of each vector image.
+
+Constants and an enumeration containing the unit name and the SVG file names provide the indices of the associated source code in the list. This makes it easier to find your way around and manage several SVG lists in a single project.
+
+*****************
+* Publisher info
+
+This application was developed by Patrick Prémartin in Delphi.
+
+It is published by OLF SOFTWARE, a company registered in Paris (France) under the reference 439521725.
+
+****************
+* Personal data
+
+This program is autonomous in its current version. It does not depend on the Internet and communicates nothing to the outside world.
+
+We have no knowledge of what you do with it.
+
+No information about you is transmitted to us or to any third party.
+
+We use no cookies, no tracking, no stats on your use of the application.
+
+***************
+* User support
+
+If you have any questions or require additional functionality, please leave us a message on the application''s website or on its code repository.
+
+To find out more, visit https://svgfolder2delphiunit.olfsoftware.fr/
+''';
+
+implementation
+
+end.
