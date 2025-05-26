0f959c402ac8b60d8534e55e5e6e5b49c54103e0
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-05-26 17:02:41 +0200

    updated C2PP comments

--- src/SVGFolder2DelphiUnit.dpr
+++ src/SVGFolder2DelphiUnit.dpr
@@ -1,54 +1,54 @@
-﻿/// <summary>
-/// ***************************************************************************
-///
-/// SVG folder to Delphi unit
-///
-/// Copyright 2024 Patrick Prémartin under AGPL 3.0 license.
-///
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-/// DEALINGS IN THE SOFTWARE.
-///
-/// ***************************************************************************
-///
-/// This program is designed for Delphi developers wishing to use vector
-/// images (in SVG format) into their projects.
-///
-/// It generates a unit from the list of SVG files contained in a folder.
-///
-/// An SVG list is created as an array of multilines strings containing the
-/// textual source code of each vector image.
-///
-/// Constants and an enumeration containing the unit name and the SVG file
-/// names provide the indices of the associated source code in the list. This
-/// makes it easier to find your way around and manage several SVG lists in a
-/// single project.
-///
-/// You can use the result in a VCL or FireMonkey Delphi project, with Skia
-/// enabled (via Skia4Delphi project or natively) and the unit
-/// Olf.Skia.SVGToBitmap.pas from
-/// https://github.com/DeveloppeurPascal/librairies/
-///
-/// ***************************************************************************
-///
-/// Author(s) :
-/// Patrick PREMARTIN
-///
-/// Site :
-/// https://svgfolder2delphiunit.olfsoftware.fr/
-///
-/// Project site :
-/// https://github.com/DeveloppeurPascal/SVGFolder2DelphiUnit
-///
-/// ***************************************************************************
-/// File last update : 2024-08-06T18:35:46.000+02:00
-/// Signature : 247601bcf70a5c8dbaa6f2dae3dd7e6011d6311c
-/// ***************************************************************************
-/// </summary>
+﻿(* C2PP
+  ***************************************************************************
+
+  SVG folder to Delphi unit
+
+  Copyright 2024-2025 Patrick Prémartin under AGPL 3.0 license.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+  DEALINGS IN THE SOFTWARE.
+
+  ***************************************************************************
+
+  This program is designed for Delphi developers wishing to use vector
+  images (in SVG format) into their projects.
+
+  It generates a unit from the list of SVG files contained in a folder.
+
+  An SVG list is created as an array of multilines strings containing the
+  textual source code of each vector image.
+
+  Constants and an enumeration containing the unit name and the SVG file
+  names provide the indices of the associated source code in the list. This
+  makes it easier to find your way around and manage several SVG lists in a
+  single project.
+
+  You can use the result in a VCL or FireMonkey Delphi project, with Skia
+  enabled (via Skia4Delphi project or natively) and the unit
+  Olf.Skia.SVGToBitmap.pas from
+  https://github.com/DeveloppeurPascal/librairies/
+
+  ***************************************************************************
+
+  Author(s) :
+  Patrick PREMARTIN
+
+  Site :
+  https://svgfolder2delphiunit.olfsoftware.fr/
+
+  Project site :
+  https://github.com/DeveloppeurPascal/SVGFolder2DelphiUnit
+
+  ***************************************************************************
+  File last update : 2025-05-25T15:38:39.071+02:00
+  Signature : 11b9c7dd744172442f3823dd65f7d643e809b738
+  ***************************************************************************
+*)
 
 program SVGFolder2DelphiUnit;
 
--- src/fMain.pas
+++ src/fMain.pas
@@ -1,54 +1,54 @@
-/// <summary>
-/// ***************************************************************************
-///
-/// SVG folder to Delphi unit
-///
-/// Copyright 2024 Patrick Prémartin under AGPL 3.0 license.
-///
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-/// DEALINGS IN THE SOFTWARE.
-///
-/// ***************************************************************************
-///
-/// This program is designed for Delphi developers wishing to use vector
-/// images (in SVG format) into their projects.
-///
-/// It generates a unit from the list of SVG files contained in a folder.
-///
-/// An SVG list is created as an array of multilines strings containing the
-/// textual source code of each vector image.
-///
-/// Constants and an enumeration containing the unit name and the SVG file
-/// names provide the indices of the associated source code in the list. This
-/// makes it easier to find your way around and manage several SVG lists in a
-/// single project.
-///
-/// You can use the result in a VCL or FireMonkey Delphi project, with Skia
-/// enabled (via Skia4Delphi project or natively) and the unit
-/// Olf.Skia.SVGToBitmap.pas from
-/// https://github.com/DeveloppeurPascal/librairies/
-///
-/// ***************************************************************************
-///
-/// Author(s) :
-/// Patrick PREMARTIN
-///
-/// Site :
-/// https://svgfolder2delphiunit.olfsoftware.fr/
-///
-/// Project site :
-/// https://github.com/DeveloppeurPascal/SVGFolder2DelphiUnit
-///
-/// ***************************************************************************
-/// File last update : 2024-08-06T19:35:58.000+02:00
-/// Signature : 0416e7376d259f697b66855e4ec033d0ca9db12c
-/// ***************************************************************************
-/// </summary>
+(* C2PP
+  ***************************************************************************
+
+  SVG folder to Delphi unit
+
+  Copyright 2024-2025 Patrick Prémartin under AGPL 3.0 license.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+  DEALINGS IN THE SOFTWARE.
+
+  ***************************************************************************
+
+  This program is designed for Delphi developers wishing to use vector
+  images (in SVG format) into their projects.
+
+  It generates a unit from the list of SVG files contained in a folder.
+
+  An SVG list is created as an array of multilines strings containing the
+  textual source code of each vector image.
+
+  Constants and an enumeration containing the unit name and the SVG file
+  names provide the indices of the associated source code in the list. This
+  makes it easier to find your way around and manage several SVG lists in a
+  single project.
+
+  You can use the result in a VCL or FireMonkey Delphi project, with Skia
+  enabled (via Skia4Delphi project or natively) and the unit
+  Olf.Skia.SVGToBitmap.pas from
+  https://github.com/DeveloppeurPascal/librairies/
+
+  ***************************************************************************
+
+  Author(s) :
+  Patrick PREMARTIN
+
+  Site :
+  https://svgfolder2delphiunit.olfsoftware.fr/
+
+  Project site :
+  https://github.com/DeveloppeurPascal/SVGFolder2DelphiUnit
+
+  ***************************************************************************
+  File last update : 2025-05-25T15:38:38.545+02:00
+  Signature : b6980555d3de93b88b779e2b0a09039cef3e34d5
+  ***************************************************************************
+*)
 
 unit fMain;
 
